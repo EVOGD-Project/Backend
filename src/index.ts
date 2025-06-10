3f84f42f9fd022d476b76915f8fbadd010a04275
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2025-06-10 09:33:06 +0100

    avatar endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import { Client as MinIOClient } from 'minio';
 import { DatabaseClient } from './classes/DatabaseClient';
 import { MainServer } from './classes/MainServer';
-
 const db = new DatabaseClient();
 
 const mainServer = new MainServer();
@@ -10,3 +10,11 @@ mainServer.setup();
 mainServer.listen();
 
 db.connect(process.env['MONGODB_URI'] ?? '');
+
+export const minio = new MinIOClient({
+	endPoint: process.env['MINIO_ENDPOINT'] ?? '',
+	port: process.env['MINIO_PORT'] ? parseInt(process.env['MINIO_PORT'] ?? '') : undefined,
+	useSSL: process.env['MINIO_USE_SSL'] ? process.env['MINIO_USE_SSL'] === 'true' : true,
+	accessKey: process.env['MINIO_USER'] ?? '',
+	secretKey: process.env['MINIO_PASSWORD'] ?? ''
+});
